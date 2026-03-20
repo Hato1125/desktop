eee936ca25747323329bfb253c9c7e89008af25c
Hato1125 <hato.flavius@gmail.com>
2026-03-20 17:22:43 +0900

    fix: missing VPN service entry

---
+++ src/service/vpn.ts
@@ -0,0 +1,44 @@
+import AstalNetwork from 'gi://AstalNetwork?version=0.1';
+
+import NM from 'gi://NM';
+import GObject, { register, property } from "ags/gobject"
+import fetch from 'gnim/fetch';
+
+@register()
+export class VpnService extends GObject.Object {
+  @property(Boolean) connected: boolean = false;
+  @property(String) country: string = '';
+
+  constructor() {
+    super();
+
+    const client = AstalNetwork.get_default().client;
+
+    client.connect('notify::active-connections', () => {
+      this.update(client);
+    });
+
+    this.update(client);
+  }
+
+  private async update(client: NM.Client) {
+    try {
+      const connections = client.get_active_connections();
+      const vpnConn = connections.find(
+        c => c.type === 'wireguard' && c.state === NM.ActiveConnectionState.ACTIVATED
+      );
+      const isConnected = !!vpnConn;
+
+      if (!isConnected) {
+        this.connected = false;
+        this.country = '';
+      } else if (!this.connected) {
+        this.connected = true;
+        const res = await fetch('http://ip-api.com/line/?fields=countryCode')
+        this.country = (await res.text()).trim();
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
