a616ab773d28d11d5a09b577f2fb503ae7c10c38
Hato1125 <hato.flavius@gmail.com>
2026-04-21 23:47:53 +0900

    perf(datetime): align tick to minute boundary instead of polling every second

--- src/ui/bar/component/datetime.tsx
+++ src/ui/bar/component/datetime.tsx
@@ -1,9 +1,18 @@
 import Gtk from 'gi://Gtk?version=4.0'
 import GLib from 'gi://GLib?version=2.0'
-import { createMemo } from 'ags';
-import { createPoll } from 'ags/time'
+import { createMemo, createState } from 'ags';
+import { timeout } from 'ags/time'
 
-const now = createPoll(new GLib.DateTime(), 1000, () => GLib.DateTime.new_now_local());
+const [now, setNow] = createState(GLib.DateTime.new_now_local());
+
+const tick = () => {
+  const current = GLib.DateTime.new_now_local();
+  setNow(current);
+  const delay = Math.max(1000, Math.round((60 - current.get_seconds()) * 1000));
+  timeout(delay, tick);
+};
+
+tick();
 
 export default () => (
   <box orientation={Gtk.Orientation.HORIZONTAL} spacing={8}>
